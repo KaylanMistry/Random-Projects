d8079c37195bcd65a1eef83c09108b8601812b24
KaylanMistry <kaylan.mistry@gmail.com>
2022-10-23 17:37:02 +0100

    Added Functions
    Changed all the functions in main to callable functions

--- tax_comparison/tax_comparison.py
+++ tax_comparison/tax_comparison.py
@@ -49,6 +49,46 @@ class Scraper:
         # else:
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.url)
+    
+    def find_currency_table(self):
+        cont = bot.driver.find_element_by_xpath('//table[@class="tablesorter ratesTable"]')
+        elements = cont.find_elements_by_tag_name('tr')
+        num_cont = len(elements)
+        for i in range(num_cont):
+            table = bot.driver.find_element_by_xpath('//table[@class="tablesorter ratesTable"]')
+            elements = cont.find_elements_by_tag_name('tr')[i]
+
+            new_data = elements.text
+            data.append(new_data)
+        data.pop(0)
+        return data
+
+    def get_currency_dict(self):
+        for currencies in data:
+        # The currencies are the individual currencies within the 'data' list
+            x = currencies.split() # This splits all the currencies and assigns it to the variable x
+            new_lst.append(x) # new_lst now contains each individual currency split into strings for each eg. ['Icelandic', 'Krona', '162.824469', '0.006142']    
+        for lsts in new_lst:
+            if len(lsts) == 5:
+                currency_string = [''.join(lsts[0]+' '+lsts[1]+' '+lsts[2]), float(lsts[3]), float(lsts[4])]
+                finished_lst.append(currency_string)
+                pass
+            if len(lsts) == 4:
+                currency_string1 = [''.join(lsts[0]+' '+lsts[1]), float(lsts[2]), float(lsts[3])]
+                finished_lst.append(currency_string1)
+                pass
+            if len(lsts) == 3:
+                currency_string2 = [lsts[0], lsts[1], float(lsts[2])]
+                finished_lst.append(lsts)
+                pass
+            
+            else:
+                pass
+        for fin_currency in finished_lst:
+            dict_currency = [fin_currency[0], fin_currency[-1]]
+            individual_currency.update([dict_currency])
+        return individual_currency
+
 
 
 if __name__ == '__main__':
@@ -57,57 +97,31 @@ if __name__ == '__main__':
     Things to do:
     1.
     Find a way to scrape the currency converter from the 'x-rates' site
-
+    DONE
     2.
     Create a dictionary that attaches the original country as the key and the exchange rate as the value
-
+    DONE
     3.
     a) Add the tax bracket for each country
     b) Use the dictionary to convert each bracket into pounds
     c) Create a program to calculate the amount you would be taxed per year
-    d) 
+    d) Create a function to compare this to another country
+
+    4.
+    Find a way to save the dictionary when you have scraped it initially.  (Not having to scrape the site again until an hour or so has passed)
+
+    5. 
+    Make sure it runs with no bugs and is user friendly
     
     
     '''
     bot = Scraper()
     time.sleep(3)
     data = []
-    cont = bot.driver.find_element_by_xpath('//table[@class="tablesorter ratesTable"]')
-    elements = cont.find_elements_by_tag_name('tr')
-    num_cont = len(elements)
-    for i in range(num_cont):
-        table = bot.driver.find_element_by_xpath('//table[@class="tablesorter ratesTable"]')
-        elements = cont.find_elements_by_tag_name('tr')[i]
-
-        new_data = elements.text
-        data.append(new_data)
-    data.pop(0)
+    bot.find_currency_table()
     new_lst = []
     finished_lst = []
-    for currencies in data:
-        # The currencies are the individual currencies within the 'data' list
-        x = currencies.split() # This splits all the currencies and assigns it to the variable x
-        new_lst.append(x) # new_lst now contains each individual currency split into strings for each eg. ['Icelandic', 'Krona', '162.824469', '0.006142']    
-    for lsts in new_lst:
-        if len(lsts) == 5:
-            currency_string = [''.join(lsts[0]+' '+lsts[1]+' '+lsts[2]), float(lsts[3]), float(lsts[4])]
-            finished_lst.append(currency_string)
-            pass
-        if len(lsts) == 4:
-            currency_string1 = [''.join(lsts[0]+' '+lsts[1]), float(lsts[2]), float(lsts[3])]
-            finished_lst.append(currency_string1)
-            pass
-        if len(lsts) == 3:
-            currency_string2 = [lsts[0], lsts[1], float(lsts[2])]
-            finished_lst.append(lsts)
-            pass
-        
-        else:
-            print("errrerrrrre")
-    for fin_currency in finished_lst:
-        dict_currency = [fin_currency[0], fin_currency[-1]]
-        individual_currency.update([dict_currency])
-        pass
+    bot.get_currency_dict()
     print(individual_currency)
 
     bot.driver.quit()
